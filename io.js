d1fb0d1c8e5b630039281bb7b743b4078389a223
shurakshya kharel <kharel.shurakshya@gmail.com>
2017-02-24 19:26:18 +0200

    changing names and creating rooms done

--- io.js
+++ io.js
@@ -1,18 +1,74 @@
 var http = require('http').Server();
 var io = require('socket.io')(http);
-
-
+var users=[];
+var rooms = ["friends", "news", "science", "family", "events", "technology"];
 var client = 0;
 
 io.on('connection', function(socket){
+	/* login with new name first time */ 
+	socket.on('login', function(username){
+		if(users.indexOf(username) === -1){
+			users.push(username);
+			socket.username = username;
+			socket.emit('userSet' , username);
+			client++;
+		}else{
+			socket.emit('username_status' , 'Sorry the username has been taken.');
+		}
+	});
 
+	io.emit('total_online_users', 'connected users' + client);
 
+	/* Changing NickName */ 
 
+	socket.on('changeName', function(newName){
+		var oldName = socket.username;
+		var newName = newName;
+		if(users.indexOf(oldName) > -1){
+			var index = users.indexOf(oldName);
+			users[index] = newName;
+			socket.emit('userSet', newName);
+			console.log(socket.username + " : newname : " + newName);
+			console.log(users);
+		}else{
+			socket.emit('username_status', 'Sorry the username has been taken.');
+			console.log("sorry username has been taken");
+		}
+	});
 
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
+	/* User Join room */ 
+
+	socket.on("joinRoom", function(room) {
+		if (rooms.indexOf(room.toLowerCase().replace(/\s/g, '')) > -1) {
+			room=room.toLowerCase().replace(/\s/g, '');
+			socket.join(room);
+			socket.room = room;
+			socket.emit("roomSet", "you are now connected to : " + socket.room);
+			console.log("user : " + socket.username + " , room : " + socket.room);
+			console.log(room.toLowerCase().replace(/\s/g, ''));
+		}
 
 	});
+
+
+	/* User Create newRoom */ 
+	socket.on('createNewRoom', function(newroomName){
+		console.log(newroomName + " :: new room created");
+		newroomName = newroomName.toLowerCase().replace(/\s/g, '');
+		if(rooms.indexOf(newroomName) > -1){
+			socket.emit('room_exists' , 'sorry room already exists');
+		}else{
+			rooms.push(newroomName);
+			socket.emit('newroomSet', 'new room setup');
+			console.log(rooms);
+		}
+
+	});
+
+	// socket.on('disconnect', function(){
+	// 	console.log('user disconnected');
+
+	// });
 
 });
 
--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,7 +1,106 @@
-var socket = io();
+$(document).ready(function(){
+	var socket = io();
+	var user="";
+	/* Javascript Handling */
+	$('#nameModal').modal('show');
 
 
 
+	/* Name save when button click */
+	$('#saveName').click(function(){
+		var name = $('#name').val();
+		var nameMatch = $('#nameMatch').val();
+		if(!name && !nameMatch){
+			$('#formError').show();
+			$('#formError').html("<span class='glyphicon glyphicon-question-sign'></span>Please fill all fields");
+		}
+		else if( name !== nameMatch || !name || !nameMatch){
+			$('#formError').show();
+			$('#formError').html("<span class='glyphicon glyphicon-question-sign'></span>Your name didnot match.");	
+		}else{
+			socket.emit('login', name);
+			socket.on('username_status', function(error){
+				$('#formError').show();
+				$('#formError').html(error);
+				console.log(data);
+			});
+			socket.on('userSet' , function(data){
+				user = data;
+				$('#nameModal').modal('hide');
+				$("#nickname").html(data);
+			});
+			socket.on('total_online_users', function(data){
+				console.log(data);
+			});
+				
+		}
+		return false;
+	});
+
+
+
+	/* change name popover form */
+	$('.popover-markup>.trigger').click(function(){
+		$('#hiding').toggle();
+		$('#changeNameButton').click(function(){
+			var a = $('#a').val();
+			var b = $('#b').val();
+			console.log(a +" : "+b);
+			if(!a && !b ){
+				return false;
+			}else if(a !== b){
+				return false;
+			}else{
+				socket.emit('changeName', a);
+				$('#hiding').hide();
+
+
+			}
+			return false;
+		});
+		return false;
+		
+	});
+
+
+	/* Create room */
+	var count=0; 
+	var eachroom="";
+	$('#createRoom').on('click',function(){
+		$('#roomerror').hide();
+		var newroomName = $('#newroomName').val();
+		eachroom = "<div class='col-sm-4'><img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQQvp2BfK3GLL6Dt0jSMOFv4Hv6NjMENnMedstBzEsjFNXOTpqf2erYle5z'><hr><h1>"+newroomName+"</h1></div>";
+		socket.emit('createNewRoom' , newroomName);
+		return false;
+
+	});
+
+	//append new created room to div 
+	socket.on('newroomSet', function(room){
+		$('#roomappend').append(eachroom);
+		console.log('new room done with interface');
+	});
+	// handle error while creating room 
+	socket.on('room_exists' , function(error){
+		$('#roomerror').show();
+		$('#roomerror').html(error);
+	});
+					
+
+	
+
+	/* Join room when individual room clicked */ 
+
+	$(".col-sm-4").click(function(e){
+		var roomName = $(this).find('h1').text();
+		if(user){	
+			socket.emit('joinRoom', roomName);
+
+		}
+
+	})
+
+});
 
 
 
